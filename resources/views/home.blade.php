a09c41ef923a689e6dddf1b3d6100bdcf5beee0f
Victor Escalante <victorlt.xd3@gmail.com>
2016-05-16 18:34:16 -0500

    Inicio de proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.default')
+
+@section('content')
+    <h1>Hola mundo</h1>
+@endsection
---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+  <title>Inicio</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
