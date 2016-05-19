9b3fd30795596095fa8b96e1337befac41a4631f
Victor Escalante <victorlt.xd3@gmail.com>
2016-05-19 10:48:08 -0500

    Agrege la carpeta de footbridges en la configuraciòn del archivo

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,6 +54,12 @@ return [
             'visibility' => 'public',
         ],
 
+        'footbridges' => [
+            'driver'     => 'local',
+            'root'       => storage_path('app/footbridges'),
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key'    => 'your-key',
